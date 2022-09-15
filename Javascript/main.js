6e21f98e9d1c6a9f147a4c8d98386aea71a3012d
CuBin2009 <92679938+CuBin2009@users.noreply.github.com>
2022-09-16 01:24:41 +0700

    Done project

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,10 +10,73 @@ var swiper = new Swiper(".reviews-content", {
     centeredSlides: true,
     autoplay: {
     delay: 5000,
-    disableOnInteraction: false,
+    disableOnInteraction: true,
     },
     pagination: {
     el: ".swiper-pagination",
     clickable: true,
     },
 });
+
+/* Email Js */
+function validate(){
+    let name = document.querySelector(".name");
+    let email = document.querySelector(".mail");
+    let msg= document.querySelector(".message");
+    let sendBtn = document.querySelector(".send-btn");
+    let navbar = document.querySelector(".navbar");
+
+    menu.onclick = () => {
+        navbar.classList.toggle("open-menu");
+        menu.classList.toggle("move");
+    }
+    window.onscroll = () => {
+        navbar.classList.remove("open-menu");
+        menu.classList.remove("move");
+    };
+    sendBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (name.value == "" || email.value == "" || msg.value== "" ) {
+            emptyerror();
+
+        }else{
+            sendEmail(name.value, email.value,msg.value);
+            success();
+        }
+    })
+}
+validate();
+
+function sendEmail(name, email,msg){
+    emailjs.send("service_9rhmt0c","template_qgh529n",{
+        to_name: email,
+        from_name: name,
+        message: msg,
+        });
+}
+
+function emptyerror(){
+    swal({
+        title: "Oh No...!",
+        text: "Fields cannot be empty!",
+        icon: "error",
+      });
+}
+function success(){
+    swal({
+        title: "Email sent success!",
+        text: "We try to reply in 24 hours!",
+        icon: "success",
+      });
+}
+/* header background change on scroll*/
+
+let header = document.querySelector("header");
+window.addEventListener("scroll",() => {
+    header.classList.toggle("header-active",window.scrollY > 0);
+})
+/* scroll top */
+let scrollTop = document.querySelector(".scroll-top");
+window.addEventListener("scroll",() => {
+    scrollTop.classList.toggle("scroll-active",window.scrollY > 0);
+})
